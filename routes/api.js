47e96d4f5a7684c94832825758cd9dafcd3f8bee
Fanaen <contact@fanaen.fr>
2015-12-09 13:33:31 +0100

    Refactor API router

--- routes/api.js
+++ routes/api.js
@@ -5,56 +5,71 @@ var express         = require('express'),
     network         = require(path.resolve('./src/network')),
     db              = require(path.resolve('./src/db'));
 
-// Main function --
-router.get('/sendText', function(req, res) {
-    if (req.isAuthenticated() || true) {
-        var input = req.query.input;
-        if(input) {
-            
-            // Call the AI to get content --
-            input2content(input, function(result) {
+// Functions --
+var callAI = function(req, res, input, callback) {
+    input2content(input, function(result) {
 
-                // Use the ExpressJS render template --
-                res.render('tiles/' + result.data.view, result.data, function(err, html) {
-                    if(err) {
-                        console.error(err);
-                        res.status(500).json({ error: err.message });
-                        return false;
-                    }
+        // Use the ExpressJS render template --
+        res.render('tiles/' + result.data.view, result.data, function(err, html) {
+            if(err) {
+                console.error(err);
+                res.status(500).json({ error: err.message });
+                return false;
+            }
 
-                    // Display only --
-                    if(req.query.display && req.query.display)  {
-                        if(window.syna) {
-                            window.syna.display(html);
-                            res.status(201).json({ message: "Input displayed." });
-                        }
-                        else {
-                            res.status(500).json({ error: "Missing view." });
-                        }
-                    }
-                    // Compute only --
-                    else {
-                        res.json({
-                            request: result.request,
-                            data: result.data,
-                            rendered: html
-                        });
-                    }
+            callback(req, res, result, html);
+            return false;
+        });
+    });
+};
 
-                    return false;
-                });
-            });
-        }
-        else {
-            res.status(400).json({ error: "Missing 'input' parameter." });
-        }
+var useAsDisplay = function(res, html) {
+    if(window.syna) {
+        window.syna.display(html);
+        res.status(201).json({ message: "Input displayed." });
     }
     else {
-        res.status(401).json({ error: 'Unauthorized request.' });
+        res.status(500).json({ error: "Missing view." });
     }
+};
+
+var useAsResponse = function(res, html, result) {
+    res.json({
+        request: result.request,
+        data: result.data,
+        rendered: html
+    });
+};
+
+
+// Main route --
+router.get('/sendText', function(req, res) {
+
+    // Check rights --
+    if (!req.isAuthenticated() && false) {
+        res.status(401).json({ error: 'Unauthorized request.' });
+        return;
+    }
+
+    // Check params --
+    var input = req.query.input;
+    if(!input) {
+        res.status(400).json({ error: "Missing 'input' parameter." });
+        return;
+    }
+
+    // Call the AI to get content --
+    callAI(req, res, input, function(req, res, result, html) {
+        if(req.query.display && req.query.display)  {
+            useAsDisplay(res, html);
+        }
+        else {
+            useAsResponse(res, html, result);
+        }
+    });
 });
 
-// Secondary functions --
+// Secondary reoutes --
 router.get('/info', function(req, res) {
     var localIPs = network.getLocalIPs()
         result = { ips: [], code: "" };
