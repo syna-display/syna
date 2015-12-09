5506b9d8d9e7fcb29ed10b12da7198d64970b289
Kévin Poirot <kevin.poirot@viacesi.fr>
2015-12-09 17:22:32 +0100

    R database

--- src/sync.js
+++ src/sync.js
@@ -1,7 +1,17 @@
-var sockets = [];
+var path = require('path'),
+    db = require(path.resolve('./src/db')),
+    sockets = [];
 
 exports.init = function(socket) {
     sockets.push(socket);
+
+    db.get(function(database) {
+        socket.emit("items:list", database.items.find());
+
+        socket.on("items:new", function(data) {
+            // Add and display a new item
+        });
+    });
 
     socket.on("disconnect", function() {
         sockets.splice(sockets.indexOf(socket), 1);
