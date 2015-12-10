f531c1e108295c4f691747e73f45b962223e9d7d
Fanaen <contact@fanaen.fr>
2015-12-10 15:05:31 +0100

    Auto-retry on connection refused

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -9,5 +9,18 @@ $(document).ready(function() {
     // -- Instructions --
 
     // Start tile --
-    $.get('http://localhost:' + window.config.server.port + '/api/v1/sendText?input=!info&display=true');
+    var startTile = function() {
+        $.get('http://localhost:'
+            + window.config.server.port
+            + '/api/v1/sendText?input=!info&display=true')
+         .done(function() {
+             console.log('Start tile: OK');
+         })
+         .fail(function(){
+            console.log('Error on start tile. Retry in 3 seconds.');
+            setTimeout(startTile, 3000);
+        });
+    };
+    startTile();
+
 })
