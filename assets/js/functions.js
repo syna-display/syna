83677238fc956ac1b2661de7c288725c7f1d1e3a
Melwin <melwinkieffer@gmail.com>
2015-12-15 16:51:51 +0100

    Use post to get info on nw startup

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,26 +1,26 @@
-$(document).ready(function() {
+$(document).ready(function () {
     window.syna = {};
 
     // -- Methods --
-    window.syna.display = function(html) {
+    window.syna.display = function (html) {
         $('#display').html(html);
     };
 
     // -- Instructions --
 
     // Start tile --
-    var startTile = function() {
-        $.get('http://localhost:'
-            + window.config.server.port
-            + '/api/v1/sendText?input=!info&display=true')
-         .done(function() {
-             console.log('Start tile: OK');
-         })
-         .fail(function(){
-            console.log('Error on start tile. Retry in 3 seconds.');
-            setTimeout(startTile, 3000);
-        });
+    var startTile = function () {
+        $.post('http://localhost:' + window.config.server.port + "/api/v1/sendText", {
+                "input": "!info",
+                "display": true
+            })
+            .done(function () {
+                console.log('Start tile: OK');
+            })
+            .fail(function () {
+                console.log('Error on start tile. Retry in 3 seconds.');
+                setTimeout(startTile, 3000);
+            });
     };
     startTile();
-
-})
+});
